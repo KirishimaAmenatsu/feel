84c9856163c3e26d7f4182cfc62dbe823db44763
CamelliaTse <CamelliaTse33@outlook.com>
2026-06-02 13:30:15 +0800

    删去了PKGBUILD的sha256校验

--- Formula/feel.rb
+++ Formula/feel.rb
@@ -2,8 +2,7 @@ class Feel < Formula
   desc "记录当前感受的命令行工具 / CLI tool to record your current feelings"
   homepage "https://github.com/CamelliaTse/feel"
   url "https://github.com/CamelliaTse/feel/archive/refs/tags/v1.2.1.tar.gz"
-  # sha256 is auto-updated by CI release workflow
-  sha256 "SKIP"
+  sha256 "7985ff6ea925e982d93d447deb4f50c4ab64ab7dbf2dc5eec4a87a12c2eabf60"
   license "MIT"
 
   depends_on "python@3.12"
--- PKGBUILD
+++ PKGBUILD
@@ -11,8 +11,7 @@ url="https://github.com/CamelliaTse/feel"
 license=('MIT')
 depends=('python')
 source=("feel")
-sha256sums=('039323375896387968924e22683c4d26d715018c7a62e6356b9c582f5e7bec52')
-validpgpkeys=('28E1F22CFC60A1BA17B95D11CDEABF16DC0D8711')
+sha256sums=('SKIP')
 
 package() {
     install -Dm755 "${srcdir}/feel" "${pkgdir}/usr/bin/feel"
